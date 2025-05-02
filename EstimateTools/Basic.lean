68e02d69bc7352b4ddc80ce2b68747a0c6e7307e
Terence Tao <tao@math.ucla.edu>
2025-05-02 14:43:46 -0700

    stub

--- EstimateTools/Basic.lean
+++ EstimateTools/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+import Mathlib.Algebra.Order.Module.Defs
+
+variable (R:Type*) [AddCommMonoid R] [Module ℝ R] [LinearOrder R] [PosSMulStrictMono R ℝ]
+
+example (x y z:ℝ) (h1: x ≤ 2 • y) (h2: y ≤ z) : x ≤ 2 • z := calc
+  x ≤ 2 • y := h1
+  _ ≤ 2 • z := by
+    simp only [←Nat.cast_smul_eq_nsmul]
+    apply smul_le_smul_of_nonneg_left
+    
